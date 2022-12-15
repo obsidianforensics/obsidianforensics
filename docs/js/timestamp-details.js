02a8126499843309a549b687483b8f9cd95c68c3
Ryan <ryan@obsidianforensics.com>
2022-12-15 08:58:58 -0800

    Updates

--- docs/js/timestamp-details.js
+++ docs/js/timestamp-details.js
@@ -8,21 +8,45 @@ function calcTimestamp(timestamp_type, inputMS) {
     case "epoch-seconds":
       return_ts = Math.trunc(inputMS / 1000).toString();
       break;
+    case "mac-hfs":
+      return_ts = (Math.trunc(inputMS / 1000) + 2082844800).toString();
+      break;
+    case "mac-absolute-time":
+      return_ts = (Math.trunc(inputMS / 1000) - 978307200).toString();
+      break;
     case "datetime-ticks":
       return_ts = ((inputMS*10000)+621355968000000000).toString();
       break;
     case "epoch-hex-seconds":
       return_ts = Math.trunc(inputMS / 1000).toString(16).toUpperCase();
       break;
+    case "epoch-hex-seconds-le":
+      hexBE = Math.trunc(inputMS / 1000).toString(16).toUpperCase();
+      var temp = hexBE.match(/../g);
+      temp.reverse();
+      return_ts = temp.join("");
+      break;
     case "epoch-milliseconds":
       if (typeof inputMS === 'number') {
-        return_ts = inputMS;
+        return_ts = inputMS.toString();
       } else {
         return_ts = inputMS.getTime();
       }
       break;
     case "filetime":
       return_ts = ((inputMS*10000)+11644473600*10000000).toString();
+      break;
+    case "filetime-hex":
+      ft_str = ((inputMS*10000)+11644473600*10000000).toString(16).toUpperCase().padStart(16, '0');
+      high_half = ft_str.slice(0,8);
+      low_half = ft_str.slice(8,);
+      return_ts = `${high_half}:${low_half}`;
+      break;
+    case "filetime-hex-le":
+      ft_str = ((inputMS*10000)+11644473600*10000000).toString(16).toUpperCase().padStart(16, '0');
+      high_half = ft_str.slice(0,8);
+      low_half = ft_str.slice(8,);
+      return_ts = `${low_half}:${high_half}`;
       break;
     case "ISO-string":
       now = new Date(inputMS);
@@ -37,100 +61,6 @@ function headerTimestamp(timestamp_type) {
   document.getElementById('detail-live-timestamp').innerHTML = calcTimestamp(timestamp_type, now);
   setTimeout(headerTimestamp, 85, timestamp_type)
 }
-
-function updateTimestamps() {
-    var now = new Date(), // current date
-        months = ['January', 'February', '...']; // you get the idea
-        time = now.getHours() + ':' + now.getMinutes() + ':' + now.getSeconds(), // again, you get the idea
-
-        // a cleaner way than string concatenation
-        date = [now.getDate(),
-                months[now.getMonth()],
-                now.getFullYear()].join(' ');
-
-    // set the content of the element with the ID time to the formatted string
-    //document.getElementById('time').innerHTML = [date, time].join(' / ');
-
-    const nowEpochMilliseconds=Date.now();
-
-    document.getElementById('epoch-millisecond').innerHTML = calcEpochMilliseconds(Date.now());
-    document.getElementById('epoch-second').innerHTML = calcEpochSeconds(Date.now());
-    document.getElementById('webkit').innerHTML = calcWebkit(Date.now());
-    document.getElementById('epoch-hex').innerHTML = calcEpochHexSeconds(Date.now())
-
-    // call this function again in this many ms
-    setTimeout(updateTimestamps, 100);
-}
-
-function setTableTimestamps() {
-
-    const now = new Date();
-
-    oldMs = 1262304000000;
-    nowMS = Date.now();
-    futureMs = 1893456000000;
-
-    document.getElementById('iso now').innerHTML = 'Now<br>' + now.toISOString();
-
-    document.getElementById('epoch-millisecond old').innerHTML = calcEpochMilliseconds(oldMs);
-    document.getElementById('epoch-second old').innerHTML = calcEpochSeconds(oldMs);
-    document.getElementById('webkit old').innerHTML = calcWebkit(oldMs);
-    document.getElementById('epoch-hex old').innerHTML = calcEpochHexSeconds(oldMs)
-
-    document.getElementById('epoch-millisecond now').innerHTML = calcEpochMilliseconds(nowMS);
-    document.getElementById('epoch-second now').innerHTML = calcEpochSeconds(nowMS);
-    document.getElementById('webkit now').innerHTML = calcWebkit(nowMS);
-    document.getElementById('epoch-hex now').innerHTML = calcEpochHexSeconds(nowMS)
-
-    document.getElementById('epoch-millisecond future').innerHTML = calcEpochMilliseconds(futureMs);
-    document.getElementById('epoch-second future').innerHTML = calcEpochSeconds(futureMs);
-    document.getElementById('webkit future').innerHTML = calcWebkit(futureMs);
-    document.getElementById('epoch-hex future').innerHTML = calcEpochHexSeconds(futureMs)
-
-}
-
-
-function writeTimestampCells(timestamp_str, timestamp_type, tense, split) {
-    document.getElementById(`${timestamp_type} ${tense} seconds`).innerHTML = timestamp_str.slice(0,split);
-    document.getElementById(`${timestamp_type} ${tense} fractional`).innerHTML = timestamp_str.slice(split,);
-}
-
-function setTableTimestampsFractional() {
-
-    const now = new Date();
-
-    oldMs = 1262304000000;
-    nowMS = Date.now();
-    futureMs = 1893456000000;
-
-    document.getElementById('iso now').innerHTML = now.toISOString();
-
-    writeTimestampCells(calcEpochHexSeconds(oldMs),'epoch-hex', 'past', 10);
-    writeTimestampCells(calcEpochHexSeconds(nowMS),'epoch-hex', 'now', 10);
-    writeTimestampCells(calcEpochHexSeconds(futureMs),'epoch-hex', 'future', 10);
-
-    writeTimestampCells(calcWebkit(oldMs),'webkit', 'past', 11);
-    writeTimestampCells(calcWebkit(nowMS),'webkit', 'now', 11);
-    writeTimestampCells(calcWebkit(futureMs),'webkit', 'future', 13);
-
-    writeTimestampCells(calcEpochSeconds(oldMs),'epoch-second', 'past', 10);
-    writeTimestampCells(calcEpochSeconds(nowMS),'epoch-second', 'now', 10);
-    writeTimestampCells(calcEpochSeconds(futureMs),'epoch-second', 'future', 10);
-
-    writeTimestampCells(calcEpochMilliseconds(oldMs),'epoch-millisecond', 'past', 10);
-    writeTimestampCells(calcEpochMilliseconds(nowMS),'epoch-millisecond', 'now', 10);
-    writeTimestampCells(calcEpochMilliseconds(futureMs),'epoch-millisecond', 'future', 10);
-
-    writeTimestampCells(calcFileTime(oldMs),'filetime', 'past', 11);
-    writeTimestampCells(calcFileTime(nowMS),'filetime', 'now', 11);
-    writeTimestampCells(calcFileTime(futureMs),'filetime', 'future', 11);
-
-    writeTimestampCells(calcDateTimeTicks(oldMs),'datetime-ticks', 'past', 11);
-    writeTimestampCells(calcDateTimeTicks(nowMS),'datetime-ticks', 'now', 11);
-    writeTimestampCells(calcDateTimeTicks(futureMs),'datetime-ticks', 'future', 11);
-
-}
-
 
 function updateTimestampDetailsTable(timestamp_type, nowMS) {
       const hourMS = 3600000
@@ -153,11 +83,40 @@ function updateTimestampDetailsTable(timestamp_type, nowMS) {
       document.getElementById(`d1 future iso`).innerHTML = calcTimestamp("ISO-string", nowMS+(hourMS*24));
       document.getElementById(`d7 future iso`).innerHTML = calcTimestamp("ISO-string", nowMS+(hourMS*24*7));
       document.getElementById(`d30 future iso`).innerHTML = calcTimestamp("ISO-string", nowMS+(hourMS*24*30));
-
 }
 
-//document.addEventListener("DOMContentLoaded", headerTimestamp());
-//document.addEventListener("DOMContentLoaded", updateTimestamps());
-//document.addEventListener("DOMContentLoaded", setTableTimestamps());
-//document.addEventListener("DOMContentLoaded", setTableTimestampsFractional());
-//document.addEventListener("DOMContentLoaded", headerTimestamp());
+// Fractional Seconds Table functions
+
+function writeFractionalTimestampCells(nowMS, timestamp_type, split) {
+  const relativeTimes = new Map();
+  relativeTimes
+   .set('past', 1262304000000)
+   .set('now', nowMS)
+   .set('future', 1893456000000);
+
+  for (const [tense, ms] of relativeTimes) {
+    timestamp_str = calcTimestamp(timestamp_type, ms);
+    document.getElementById(`${timestamp_type} ${tense} seconds`).innerHTML = timestamp_str.slice(0,split);
+    document.getElementById(`${timestamp_type} ${tense} fractional`).innerHTML = timestamp_str.slice(split,);
+  }
+}
+
+function setTableTimestampsFractional(nowMS) {
+
+    document.getElementById('iso now').innerHTML = calcTimestamp('ISO-string', nowMS);
+
+    const timestampSplits = new Map();
+    timestampSplits
+     .set('epoch-hex-seconds', 10)
+//     .set('epoch-hex-seconds-le', 10)
+     .set('epoch-seconds', 10)
+     .set('epoch-milliseconds', 10)
+     .set('datetime-ticks', 11)
+     .set('filetime', 11)
+     .set('webkit', 11);
+
+    for (let [timestamp_type, split] of timestampSplits) {
+      writeFractionalTimestampCells(nowMS, timestamp_type, split)
+    }
+}
+
--- docs/js/timestamps.js
+++ docs/js/timestamps.js
@@ -28,56 +28,6 @@ function headerTimestamp() {
 
   setTimeout(headerTimestamp, 85)
 }
-function updateTimestamps() {
-    var now = new Date(), // current date
-        months = ['January', 'February', '...']; // you get the idea
-        time = now.getHours() + ':' + now.getMinutes() + ':' + now.getSeconds(), // again, you get the idea
-
-        // a cleaner way than string concatenation
-        date = [now.getDate(),
-                months[now.getMonth()],
-                now.getFullYear()].join(' ');
-
-    // set the content of the element with the ID time to the formatted string
-    //document.getElementById('time').innerHTML = [date, time].join(' / ');
-
-    const nowEpochMilliseconds=Date.now();
-
-    document.getElementById('epoch-millisecond').innerHTML = calcEpochMilliseconds(Date.now());
-    document.getElementById('epoch-second').innerHTML = calcEpochSeconds(Date.now());
-    document.getElementById('webkit').innerHTML = calcWebkit(Date.now());
-    document.getElementById('epoch-hex').innerHTML = calcEpochHexSeconds(Date.now())
-
-    // call this function again in this many ms
-    setTimeout(updateTimestamps, 100);
-}
-
-function setTableTimestamps() {
-
-    const now = new Date();
-
-    oldMs = 1262304000000;
-    nowMS = Date.now();
-    futureMs = 1893456000000;
-
-    document.getElementById('iso now').innerHTML = 'Now<br>' + now.toISOString();
-
-    document.getElementById('epoch-millisecond old').innerHTML = calcEpochMilliseconds(oldMs);
-    document.getElementById('epoch-second old').innerHTML = calcEpochSeconds(oldMs);
-    document.getElementById('webkit old').innerHTML = calcWebkit(oldMs);
-    document.getElementById('epoch-hex old').innerHTML = calcEpochHexSeconds(oldMs)
-
-    document.getElementById('epoch-millisecond now').innerHTML = calcEpochMilliseconds(nowMS);
-    document.getElementById('epoch-second now').innerHTML = calcEpochSeconds(nowMS);
-    document.getElementById('webkit now').innerHTML = calcWebkit(nowMS);
-    document.getElementById('epoch-hex now').innerHTML = calcEpochHexSeconds(nowMS)
-
-    document.getElementById('epoch-millisecond future').innerHTML = calcEpochMilliseconds(futureMs);
-    document.getElementById('epoch-second future').innerHTML = calcEpochSeconds(futureMs);
-    document.getElementById('webkit future').innerHTML = calcWebkit(futureMs);
-    document.getElementById('epoch-hex future').innerHTML = calcEpochHexSeconds(futureMs)
-
-}
 
 
 function writeTimestampCells(timestamp_str, timestamp_type, tense, split) {
@@ -87,6 +37,20 @@ function writeTimestampCells(timestamp_str, timestamp_type, tense, split) {
 
 function setTableTimestampsFractional() {
 
+    var splits = {
+      'datetime-ticks': 11,
+      'epoch-hex-seconds': 10,
+//      'epoch-hex-seconds-le': 10,
+      'epoch-milliseconds': 10,
+      'epoch-seconds': 10,
+      'filetime': 11,
+      'filetime-hex': 11,
+//      'filetime-hex-le': 11,
+      'mac-absolute-time': 9,
+      'mac-hfs': 10,
+      'webkit': 11,
+      };
+
     const now = new Date();
 
     oldMs = 1262304000000;
@@ -95,29 +59,11 @@ function setTableTimestampsFractional() {
 
     document.getElementById('iso now').innerHTML = now.toISOString();
 
-    writeTimestampCells(calcEpochHexSeconds(oldMs),'epoch-hex-seconds', 'past', 10);
-    writeTimestampCells(calcEpochHexSeconds(nowMS),'epoch-hex-seconds', 'now', 10);
-    writeTimestampCells(calcEpochHexSeconds(futureMs),'epoch-hex-seconds', 'future', 10);
-
-    writeTimestampCells(calcWebkit(oldMs),'webkit', 'past', 11);
-    writeTimestampCells(calcWebkit(nowMS),'webkit', 'now', 11);
-    writeTimestampCells(calcWebkit(futureMs),'webkit', 'future', 13);
-
-    writeTimestampCells(calcEpochSeconds(oldMs),'epoch-seconds', 'past', 10);
-    writeTimestampCells(calcEpochSeconds(nowMS),'epoch-seconds', 'now', 10);
-    writeTimestampCells(calcEpochSeconds(futureMs),'epoch-seconds', 'future', 10);
-
-    writeTimestampCells(calcEpochMilliseconds(oldMs),'epoch-milliseconds', 'past', 10);
-    writeTimestampCells(calcEpochMilliseconds(nowMS),'epoch-milliseconds', 'now', 10);
-    writeTimestampCells(calcEpochMilliseconds(futureMs),'epoch-milliseconds', 'future', 10);
-
-    writeTimestampCells(calcFileTime(oldMs),'filetime', 'past', 11);
-    writeTimestampCells(calcFileTime(nowMS),'filetime', 'now', 11);
-    writeTimestampCells(calcFileTime(futureMs),'filetime', 'future', 11);
-
-    writeTimestampCells(calcDateTimeTicks(oldMs),'datetime-ticks', 'past', 11);
-    writeTimestampCells(calcDateTimeTicks(nowMS),'datetime-ticks', 'now', 11);
-    writeTimestampCells(calcDateTimeTicks(futureMs),'datetime-ticks', 'future', 11);
+    Object.keys(splits).forEach(item => {
+     writeTimestampCells(calcTimestamp(item, oldMs), item, 'past', splits[item]);
+     writeTimestampCells(calcTimestamp(item, nowMS), item, 'now', splits[item]);
+     writeTimestampCells(calcTimestamp(item, futureMs), item, 'future', splits[item]);
+     } )
 
 }
 
